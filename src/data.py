b37b83f32297fdc9f5d8de42104229891c3df19d
reggev <reggev.am@gmail.com>
2020-09-24 20:17:48 +0300

    added cat file and cli commands

--- src/data.py
+++ src/data.py
@@ -9,8 +9,13 @@ def init():
     os.makedirs(f'{GIT_DIR}/objects', exist_ok=True)
 
 
-def hash_objects(data):
+def hash_object(data):
     oid = hashlib.sha1(data).hexdigest()
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
         out.write(data)
     return oid
+
+
+def get_object(oid):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
