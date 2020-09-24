0593c1d96b2b13bec091ff3a970380a7ca498b20
reggev <reggev.am@gmail.com>
2020-09-24 22:15:55 +0300

    added parent commit entry to commits

--- src/base.py
+++ src/base.py
@@ -71,6 +71,9 @@ def read_tree(tree_oid):
 
 def commit(message):
     commit = f'tree {write_tree()}\n'
+    HEAD = data.get_HEAD()
+    if (HEAD):
+        commit += f'parent {HEAD}\n'
     commit += '\n'
     commit += f'{message}\n'
     oid = data.hash_object(commit.encode(), 'commit')
