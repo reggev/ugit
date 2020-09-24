8cd1f03648a2640a1d2d1b4e423be8e78fb01eaf
reggev <reggev.am@gmail.com>
2020-09-24 20:07:58 +0300

    improved init and implemented hash objects method

--- src/data.py
+++ src/data.py
@@ -1,7 +1,16 @@
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
 
 
 def init():
-    os.makedirs(GIT_DIR)
+    os.makedirs(GIT_DIR, exist_ok=True)
+    os.makedirs(f'{GIT_DIR}/objects', exist_ok=True)
+
+
+def hash_objects(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
