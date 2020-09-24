6a5ea33e643deac146e9090ece41e8b6c5ba45f9
reggev <reggev.am@gmail.com>
2020-09-24 22:09:33 +0300

    added HEAD

--- src/base.py
+++ src/base.py
@@ -73,4 +73,6 @@ def commit(message):
     commit = f'tree {write_tree()}\n'
     commit += '\n'
     commit += f'{message}\n'
-    return data.hash_object(commit.encode(), 'commit')
+    oid = data.hash_object(commit.encode(), 'commit')
+    data.set_HEAD(oid)
+    return oid
